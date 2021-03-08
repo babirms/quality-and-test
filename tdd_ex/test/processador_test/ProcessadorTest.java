6cd08346c65f9095b0f43a06ef648c022d285811
babirms <bramos@id.uff.br>
2021-03-08 15:54:27 -0300

    criação dos casos de teste de processador

--- tdd_ex/test/processador_test/ProcessadorTest.java
+++ tdd_ex/test/processador_test/ProcessadorTest.java
@@ -1,5 +1,47 @@
 package processador_test;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ProcessadorTest {
 
+
+	@Test
+	@DisplayName("1 - Verifica o status de pagamento de uma fatura que já foi paga.")
+	void verificaFaturaPaga() {
+
+
+
+	}
+	
+	@Test
+	@DisplayName("2 - Verifica o status de pagamento de uma fatura que não foi paga.")
+	void verificaFaturaNaoPaga() {
+
+
+	}
+	
+	@Test
+	@DisplayName("3 - Verifica se o valor de um boleto foi pago integralmente")
+	void verificaBoletoPagoIntegralmente() {
+
+	}
+	
+	@Test
+	@DisplayName(" 4 - Verifica se o valor de um boleto não foi pago integralmente")
+	void verificaBoletoNaoPagoIntegralmente() {
+
+	}
+	
+	@Test
+	@DisplayName("5 - Verifica se o pagamento de um boleto é válido a partir da sua data de validade")
+	void verificaBoletoValido() {
+
+	}
+	
+	@Test
+	@DisplayName("6 - Verifica se o pagamento de um boleto não é válido a partir da sua data de validade")
+	void verificaBoletoInvalido() {
+
+	}
 }
